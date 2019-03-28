d2d0dd31b92e4c3485eff724f644b9011da04c6a
Lucchetto <lucchetto.tie@live.com>
2019-03-28 18:41:03 +0100

    base: Smart Charging (1/4)
    Change-Id: Ifd9e1d8094287e145870510b2f9e85829c211603

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5276,6 +5276,17 @@ public final class Settings {
         public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";        
 
         /**
+         * @hide
+         */
+        public static final String SMART_CHARGING = "smart_charging";
+
+        /**
+         * @hide
+         */
+        public static final String SMART_CHARGING_LEVEL = "smart_charging_level";
+
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
