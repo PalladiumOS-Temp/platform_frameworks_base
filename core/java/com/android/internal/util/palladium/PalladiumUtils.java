26b9bb633231ae89d2f1b2f510ed465f42d6a059
BoredOutOfMyGit <BoredOutOfMyGit@users.noreply.github.com>
2020-09-29 19:57:39 +0000

    Utils: Add method to determine battery temp
    Change-Id: Id0b8addc460afae030eac0a2bf064fb21aa2c4dc
    
    Signed-off-by: Sagarrokade006 <sagarrokade006@gmail.com>

--- core/java/com/android/internal/util/palladium/PalladiumUtils.java
+++ core/java/com/android/internal/util/palladium/PalladiumUtils.java
@@ -20,10 +20,12 @@ import android.Manifest;
 import android.content.Context;
 import android.app.ActivityManager;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.hardware.fingerprint.FingerprintManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.BatteryManager;
 import android.os.SystemProperties;
 import android.hardware.input.InputManager;
 import android.os.Handler;
@@ -102,6 +104,19 @@ public class PalladiumUtils {
     // Check to see if device supports A/B (seamless) system updates
     public static boolean isABdevice(Context context) {
         return SystemProperties.getBoolean("ro.build.ab_update", false);
+    }
+
+    public static String batteryTemperature(Context context, Boolean ForC) {
+        Intent intent = context.registerReceiver(null, new IntentFilter(
+                Intent.ACTION_BATTERY_CHANGED));
+        float  temp = ((float) (intent != null ? intent.getIntExtra(
+                BatteryManager.EXTRA_TEMPERATURE, 0) : 0)) / 10;
+        // Round up to nearest number
+        int c = (int) ((temp) + 0.5f);
+        float n = temp + 0.5f;
+        // Use boolean to determine celsius or fahrenheit
+        return String.valueOf((n - c) % 2 == 0 ? (int) temp :
+                ForC ? c * 9/5 + 32 + "°F" :c + "°C");
     }
 
     public static void switchScreenOff(Context ctx) {
