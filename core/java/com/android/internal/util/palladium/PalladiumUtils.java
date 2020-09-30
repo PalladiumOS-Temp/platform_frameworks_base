ed4197f21d64540a01cfa2de77b14999cc9f0e45
Genkzsz11 <Genkzsz11@gmail.com>
2020-09-30 17:27:31 +0700

    PalladiumUtils: Add missing systemUI resources in isThemeEnabled -- Pick from https://github.com/DirtyUnicorns/android_frameworks_base/commit/3e96b0b868eb32b30fe5b40b1fb3aea5c5d935e2#diff-d076d2c9ae5697840f02efd22836dc65
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/com/android/internal/util/palladium/PalladiumUtils.java
+++ core/java/com/android/internal/util/palladium/PalladiumUtils.java
@@ -63,6 +63,7 @@ import com.android.internal.R;
 import com.android.internal.statusbar.IStatusBarService;
 
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -71,8 +72,7 @@ import java.util.Locale;
  */
 public class PalladiumUtils {
 
-    private static OverlayManager mOverlayService;
-
+    private static OverlayManager sOverlayService;
     public static final String INTENT_SCREENSHOT = "action_handler_screenshot";
     public static final String INTENT_REGION_SCREENSHOT = "action_handler_region_screenshot";
 
@@ -320,10 +320,15 @@ public class PalladiumUtils {
 
     // Method to detect whether an overlay is enabled or not
     public static boolean isThemeEnabled(String packageName) {
-        mOverlayService = new OverlayManager();
+        if (sOverlayService == null) {
+            sOverlayService = new OverlayManager();
+        }
         try {
-            List<OverlayInfo> infos = mOverlayService.getOverlayInfosForTarget("android",
-                    UserHandle.myUserId());
+            ArrayList<OverlayInfo> infos = new ArrayList<OverlayInfo>();
+            infos.addAll(sOverlayService.getOverlayInfosForTarget("android",
+                    UserHandle.myUserId()));
+            infos.addAll(sOverlayService.getOverlayInfosForTarget("com.android.systemui",
+                    UserHandle.myUserId()));
             for (int i = 0, size = infos.size(); i < size; i++) {
                 if (infos.get(i).packageName.equals(packageName)) {
                     return infos.get(i).isEnabled();
