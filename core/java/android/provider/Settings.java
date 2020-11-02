7163dca6a36dfa7f31ccd1327d6897796f38ccb2
calebcabob <calphonic@gmail.com>
2020-11-02 12:01:31 +0530

    NetworkTraffic: Allow user to choose positioning [1/2]
    This allows the user to choose whether or not they want to see the traffic indicator in status bar or in the expanded header
    
    Change-Id: Ifff10bcccde91ea90f7bfe85acfe70ffc54a9418
    Signed-off-by: Zaviyar786 <beingme639@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5333,6 +5333,15 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_HIDEARROW = "network_traffic_hidearrow";
 
         /**
+         * Show network traffic indicator
+         * 0 - Statusbar (default)
+         * 1 - QS Header
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_VIEW_LOCATION = "network_traffic_view_location";
+
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -5480,6 +5489,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(ALLOW_INCALL_HOME);
             PRIVATE_SETTINGS.add(QS_PANEL_BG_USE_NEW_TINT);
             PRIVATE_SETTINGS.add(SYSTEMUI_PLUGIN_VOLUME);
+            PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_VIEW_LOCATION);
         }
 
         /**
