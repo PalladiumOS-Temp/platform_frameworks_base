8786f91883446d9b555bfd2e082515681ce5af99
00day0 <therandomuser11@gmail.com>
2020-12-26 14:40:11 +0300

    FODCircleView: Recognizing fingerprint animation [1/2]
    Change-Id: I86a49d2d8f7607d0f81b84e7e751930309ec4a1f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5336,6 +5336,12 @@ public final class Settings {
 
 
         /**
+         * FOD recognizing animation
+         * @hide
+         */
+        public static final String FOD_RECOGNIZING_ANIMATION = "fod_recognizing_animation";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
