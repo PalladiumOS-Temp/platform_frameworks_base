PalladiumOS-Temp/platform_frameworks_base

commit 20cdddf29e0b76e05b80d786988fca12226da224

Lockscreen MediaArt: hide if media is not playing
it's annoying to get the artwork even if the notification gets
swiped out and media is paused, because the media session is still alive

Change-Id: I3540e1429524d34029d273c5cbe216fa46e4d59e

packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
@@ -658,7 +658,9 @@ public class NotificationMediaManager implements Dumpable {
             }
         }
 
-        if ((hasArtwork || DEBUG_MEDIA_FAKE_ARTWORK)
+        // show artwork only if the media is playing
+        if (PlaybackState.STATE_PLAYING == getMediaControllerPlaybackState(mMediaController)
+                && (hasArtwork || DEBUG_MEDIA_FAKE_ARTWORK)
                 && (mStatusBarStateController.getState() != StatusBarState.SHADE || allowWhenShade)
                 &&  mBiometricUnlockController != null && mBiometricUnlockController.getMode()
                         != BiometricUnlockController.MODE_WAKE_AND_UNLOCK_PULSING

commit 169a92eab96a8f1dcf4d76421c8b8cb005f46450

Fix lockscreen wallpaper not being set if different from the Home one
this got broken with 20cdddf29e0b76e05b80d786988fca12226da224

thanks to @anayw2001 who digged into commits chain and found
the root of the issue

Change-Id: Ifc96f6e540897a43fdd3cf23c8ac518a7f2f77e2

Signed-off-by: Lokesh773 <lokeshkasamneni773@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
@@ -615,7 +615,9 @@ public class NotificationMediaManager implements Dumpable {
     private void finishUpdateMediaMetaData(boolean metaDataChanged, boolean allowEnterAnimation,
             @Nullable Bitmap bmp) {
         Drawable artworkDrawable = null;
-        if (bmp != null) {
+        // set media artwork as lockscreen wallpaper if player is playing
+        if (bmp != null && (mLockscreenArt || !ENABLE_LOCKSCREEN_WALLPAPER) &&
+                PlaybackState.STATE_PLAYING == getMediaControllerPlaybackState(mMediaController)) {
             artworkDrawable = new BitmapDrawable(mBackdropBack.getResources(), bmp);
         }
         boolean hasMediaArtwork = artworkDrawable != null;
@@ -658,9 +660,7 @@ public class NotificationMediaManager implements Dumpable {
             }
         }
 
-        // show artwork only if the media is playing
-        if (PlaybackState.STATE_PLAYING == getMediaControllerPlaybackState(mMediaController)
-                && (hasArtwork || DEBUG_MEDIA_FAKE_ARTWORK)
+        if ((hasArtwork || DEBUG_MEDIA_FAKE_ARTWORK)
                 && (mStatusBarStateController.getState() != StatusBarState.SHADE || allowWhenShade)
                 &&  mBiometricUnlockController != null && mBiometricUnlockController.getMode()
                         != BiometricUnlockController.MODE_WAKE_AND_UNLOCK_PULSING